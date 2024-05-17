27282a0ce014503bfd43e20b96b10eaa1d5bfc25
sunghee.kim <sunghee3.kim@gmail.com>
2024-05-17 13:53:03 +0900

    Add exception case of no number

--- test/Sample-Test1/test.cpp
+++ test/Sample-Test1/test.cpp
@@ -5,3 +5,8 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
     Baseball game;
     EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+    Baseball game;
+    EXPECT_THROW(game.guess(string("12s")), length_error);
+}
