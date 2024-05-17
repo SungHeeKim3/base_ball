7cfcc356982025b960c5cda0204c916ea9eaedda
sunghee.kim <sunghee3.kim@gmail.com>
2024-05-17 14:09:11 +0900

    finish pdf provide

--- test/Sample-Test1/test.cpp
+++ test/Sample-Test1/test.cpp
@@ -3,7 +3,7 @@
 
 class BassballFixture : public testing::Test {
 public:
-    Baseball game;
+    Baseball game{ "123" };
     void assertIllegalArgument(string guessNumber)
     {
         try {
@@ -21,4 +21,12 @@ TEST_F(BassballFixture, ThrowExceptionWhenInvalidCase) {
     assertIllegalArgument("12");
     assertIllegalArgument("12s");
     assertIllegalArgument("121");
+}
+
+TEST_F(BassballFixture, ReturnSolveResultIfMatcheNumber) {
+    GuessResult result = game.guess("123");
+
+    EXPECT_TRUE(result.solved);
+    EXPECT_EQ(3, result.strikes);
+    EXPECT_EQ(0,result.balls);
 }
